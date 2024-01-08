a104cb0fb16800de3f4eee3bd74d03c0746381b1
nuhanordin <nuhaandn@gmail.com>
2024-01-08 22:31:05 +0800

    added first

---
+++ TrackWallet/app/src/main/java/com/example/trackwallet/model/User.kt
@@ -0,0 +1,8 @@
+package com.example.trackwallet.model
+
+data class User(
+    val userId: String? = null,
+    val username: String? = null,
+    val email: String? = null,
+    val password: String? = null
+)
