431de8052e13be32f7de0470480c666b62393ad0
nuhanordin <nuhaandn@gmail.com>
2024-01-09 11:54:23 +0800

    updated login register

--- app/src/main/java/com/example/trackwallet/MainActivity.kt
+++ app/src/main/java/com/example/trackwallet/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.trackwallet
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 
@@ -10,6 +11,7 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var textUsername: TextView
     lateinit var textEmail: TextView
+    lateinit var buttonLogout: Button
 
     val firebaseAuth = FirebaseAuth.getInstance()
 
@@ -18,12 +20,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         textUsername = findViewById(R.id.username)
         textEmail = findViewById(R.id.email)
+        buttonLogout = findViewById(R.id.buttonLogout)
 
         val firebaseUser = firebaseAuth.currentUser
         if (firebaseUser != null) {
             textUsername.text = firebaseUser.displayName
             textEmail.text = firebaseUser.email
         } else {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
+
+        buttonLogout.setOnClickListener {
+            firebaseAuth.signOut()
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }
